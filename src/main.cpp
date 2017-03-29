01d9c13d274105e6f94312b3e6f535edae6cb4e7
marcosRiarte <mriarte@fi.uba.ar>
2017-03-29 01:43:21 -0300

    Probando Travis  y Clion

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 int main() {
 	std::cout << "Hola mundo!" << std::endl;
 	std::cout << "Probando repositorios Git con CLion" << std::endl;
+	std::cout << "Probando  nuevo fork personal " << std::endl;
 
 	return 0;
 }
