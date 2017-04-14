e9007cb88b71f11843efa795b6f73c1010308a0d
Cesar Leguizamon <fabi1816@gmail.com>
2017-04-14 12:25:41 -0300

    Avanzamos un poco con el main y el setteo de los jugadores

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,26 @@ int main() {
 
 	std::vector<int> cartasJ1 = { 1, 2, 3, 4 };
 	std::vector<int> cartasJ2 = { 1, 2, 3, 4 };
+	std::vector< std::vector<int> > cartas = { cartasJ1, cartasJ2 };
 
 	player::Turno t1("/TurnoJugador1", "/TurnoJugador2");
 	player::Turno t2("/TurnoJugador2", "/TurnoJugador1");
+	std::vector<player::Turno> turnos = { t1, t2 };
+
 	std::cout << "Se crearon los semaforos" << std::endl;
 
-	player::Jugador j1(cartasJ1, t1);
-	player::Jugador j2(cartasJ2, t2);
+	for (int i = 0; i < 2; ++i) {
+		pid_t pid = fork();
+		if (pid == 0) {
+			player::Jugador j(cartas[i], turnos[i]);
+			return j.Jugar();
+		}
+	}
+
+	for (int i = 0; i < 2; ++i) {
+		wait();
+	}
+
 	std::cout << "Se crearon los jugadores" << std::endl;
 
 	return 0;
