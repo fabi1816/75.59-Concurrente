fe08f5118e24e8217772e2ad98fed9d9878ec1ac
fabi1816 <fabi1816@gmail.com>
2017-04-24 00:47:49 -0300

    Prueba de semaforo que funciona correctamente

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/shm.h>
+#include <sys/sem.h>
 #include <sys/wait.h>
-#include <errno.h>
 
 #include <string>
 #include <iostream>
+#include <stdexcept>
+#include <system_error>
+#include <cerrno>
 
 #include "Jugador.h"
 #include "Turno.h"
@@ -15,91 +16,88 @@
 
 
 int main() {
-	std::cout << "Prueba de juego - mkIII" << std::endl;
+	try {
+		std::cout << "Prueba de juego - mkIV" << std::endl;
 
-	// Las cartas del jugador
-	std::vector<int> cartasJ1 = { 1, 2 };
+		// Las cartas del jugador
+		std::vector<int> cartasJ1 = { 1, 2 };
 
-	// Mover el semaforo a memoria compartida
-	key_t k = ftok("/bin/ls", 19);
-	if (k == -1) {
-		std::cout << "** Error al crear la key de la memoria compartida" << std::endl;
-		return -1;
-	}
+		// Crear semaforo
+		key_t k = ftok("/bin/ls", 19);
+		if (k == -1) {
+			throw std::system_error(errno, std::generic_category(), "Falló la creación de una key");
+		}
 
-	int memID = shmget(k, sizeof(sem_t), IPC_CREAT | IPC_EXCL | 0666);
-	if (memID == -1) {
-		std::cout << "** Error al alocar la memoria compartida " << std::endl;
+		int semID = semget(k, 1, IPC_CREAT | 0644);
+		if (semID == -1) {
+			throw std::system_error(errno, std::generic_category(), "Falló la creación de los semaforos");
+		}
+
+		// Init semaforo
+		union semun {
+			int val;
+			struct semid_ds* b;
+			unsigned short* ar;
+			struct seminfo* in;
+		};
+		semun init = { 0 };
+		int res = semctl(semID, 0, SETVAL, init);
+		if (res == -1) {
+			throw std::system_error(errno, std::generic_category(), "Falló la inicializacion de los semaforos");
+		}
+
+		std::cout << "Semaforo listo" << std::endl;
+
+		// Se crea el jugador de prueba en su propio proceso
+		pid_t pid = fork();
+		if (pid == 0) {
+			std::cout << "--> Se crea al jugagor" << std::endl;
+			std::cout << "--> Espero..." << std::endl;
+
+			sembuf sops = { };
+			sops.sem_num = 0;
+			sops.sem_op = -1;	// Wait
+			res = semop(semID, &sops, 1);
+			if (res == -1) {
+				throw std::system_error(errno, std::generic_category(), "Falló la espera del semaforo");
+			}
+			
+			std::cout << "--> Juego una carta!" << std::endl;
+			std::cout << "--> Señalo al siguiente" << std::endl;
+
+			return 0;
+		}
+
+		// Señala al primer jugador que comienze el juego
+		sleep(3);
+		std::cout << "Empieza el juego" << std::endl;
+		sembuf sops = { };
+		sops.sem_num = 0;
+		sops.sem_op = 1;	// Signal
+		res = semop(semID, &sops, 1);
+		if (res == -1) {
+			throw std::system_error(errno, std::generic_category(), "Falló la señal del semaforo");
+		}
+
+		// Se espera por el jugador
+		std::cout << "Espero por un jugador" << std::endl;
+		int stat = 0;
+		wait(&stat);
+
+		// Libero el semaforo
+		res = semctl(semID, 0, IPC_RMID);
+		if (res == -1) {
+			throw std::system_error(errno, std::generic_category(), "Falló la eliminación del semaforo");
+		}
+
+		std::cout << "Fin del juego" << std::endl;
+
+		return 0;
+
+	} catch (const std::system_error e) {
+		std::cout << "** Error: " << e.code() << " -> " << e.what() << std::endl;
 		perror(nullptr);
 		return -1;
 	}
-
-	void* shMem = shmat(memID, nullptr, 0);
-	if (shMem == (void*) -1) {
-		std::cout << "** Error al atachar la memoria compartida" << std::endl;
-		return -1;
-	}
-
-	// El semaforo que lleva cuenta del turno
-	sem_t* turno1 = (sem_t*) shMem;
-	int res = sem_init(turno1, 1, 0);
-	if (res == -1) {
-		std::cout << "** Error al crear el semaforo" << std::endl;
-		return -1;
-	}
-
-	std::cout << "Semaforo listo" << std::endl;
-
-	// Se crea el jugador de prueba en su propio proceso
-	pid_t pid = fork();
-	if (pid == 0) {
-		std::cout << "Se crea al jugagor" << std::endl;
-		std::cout << "Espero..." << std::endl;
-		sem_wait(turno1);
-		std::cout << "Juego una carta!" << std::endl;
-		sem_post(turno1);
-		std::cout << "Señalo al siguiente" << std::endl;
-
-		//player::Jugador j(cartasJ1, t1);
-		//return j.Jugar();
-	}
-
-	// Señala al primer jugador que comienze el juego
-	std::cout << "Empieza el juego" << std::endl;
-	res = sem_post(turno1);
-	if (res == -1) {
-		std::cout << "** Error al postear un semaforo" << std::endl;
-		return -1;
-	}
-
-
-	// Se espera por el jugador
-	std::cout << "Espero por un jugador" << std::endl;
-	int stat = 0;
-	wait(&stat);
-
-	// Libero el semaforo
-	res = sem_destroy(turno1);
-	if (res == -1) {
-		std::cout << "** Error al eliminar un semaforo" << std::endl;
-		return -1;
-	}
-
-	// Liberar la memoria compartida
-	res = shmctl(memID, IPC_RMID, nullptr);
-	if (res == -1) {
-		std::cout << "** Error al borrar la memoria compartida" << std::endl;
-		return -1;
-	}
-
-	res = shmdt(shMem);
-	if (res == -1) {
-		std::cout << "** Error al desatachar la memoria compartida" << std::endl;
-		return -1;
-	}
-
-	std::cout << "Fin del juego" << std::endl;
-
-	return 0;
 }
 
