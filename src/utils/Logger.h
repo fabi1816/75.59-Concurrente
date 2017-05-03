34f75d30283f43323919a2e597bf20fb66a31f58
marcosRiarte <mriarte@fi.uba.ar>
2017-05-03 06:21:26 -0300

    Interfaz de loger

--- src/utils/Logger.h
+++ src/utils/Logger.h
@@ -1,10 +1,18 @@
 #ifndef LOGGER_H
 #define LOGGER_H
-
+//Clase que simula el log del juego
 
 namespace utils {
 
 	class Logger {
+        //Recibe el path del archivo donde se guardaran las acciones
+		Logger(string path);
+
+        //Escribe la fecha y hora,el id del jugdor y mensaje en el archivo y muestra por consola
+        void escribir (int IdJugador,string mensaje);
+
+        //Destructor
+        ~ Logger();
 
 	};
 }
