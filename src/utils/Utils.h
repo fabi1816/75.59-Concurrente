13409a00150666e90a6076d203babc3d24f3f688
Cesar Leguizamon <fabi1816@gmail.com>
2017-05-15 00:23:11 -0300

    Se quitaron metodos no utilizados

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -1,15 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <sys/wait.h>
 
 #include <string>
-#include <sstream>
-#include <iostream>
 #include <stdexcept>
 #include <system_error>
 #include <cerrno>
@@ -20,9 +17,6 @@ namespace utils {
 	// Checks for errors in system calls
 	void checkError(int result, std::string errMsg);
 	void checkErrorWithInteruption(int result, std::string errMsg);
-
-	// Checks errors when waiting for processes and returns messages
-	std::string waitHandler(int pid, int stat, int slowSignal);
 
 	// Creates a semaphore set
 	int createSemaphoreSet(char uid, int cant);
